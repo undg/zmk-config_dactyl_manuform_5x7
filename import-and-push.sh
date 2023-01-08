198518d7b1498509457caa4dfb7a65dd7f1738f8
undg <und3rdg@gmail.com>
2023-01-08 00:11:29 +0000

    build_date +%Y-%m-%d_%H-%m_%S

--- import-and-push.sh
+++ import-and-push.sh
@@ -2,5 +2,5 @@
 
 now="date +%Y-%m-%d_%H-%m_%S"
 
- cp ~/Code/zmk/app/boards/shields/dactyl_manuform_5x7/dactyl_manuform_5x7.keymap config/dactylM.keymap; git add .; git cm -m "build_$(now)"; git push
+ cp ~/Code/zmk/app/boards/shields/dactyl_manuform_5x7/dactyl_manuform_5x7.keymap config/dactylM.keymap; git add .; git cm -m "build_$now"; git push
 

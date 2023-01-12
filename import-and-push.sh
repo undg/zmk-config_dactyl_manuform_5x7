6f806c6e7cfec42c0fdb3e075b059b3234097984
undg <und3rdg@gmail.com>
2023-01-12 23:49:01 +0000

    build_2023-01-12_23-01_01

--- import-and-push.sh
+++ import-and-push.sh
@@ -2,5 +2,9 @@
 
 now=$(date +%Y-%m-%d_%H-%m_%S)
 
+cd ~/Code/kbd-dactyl_manuform_5x7-zmk-config/
+
 cp ~/Code/zmk/app/boards/shields/dactyl_manuform_5x7/dactyl_manuform_5x7.keymap config/dactylM.keymap; git add .; git cm -m "build_$now"; git push
+
+cd -
 
